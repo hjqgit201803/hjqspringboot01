a9a2ff43bebf9e747f3fe7127a8e584bed508c7c
hjq001git <1315251784@qq.com>
2018-10-28 22:44:55 +0800

    dao接口的实现-测试

--- springboot-simple-example/src/main/java/com/hjq/springboot/springbootsimpleexample/dao/OrderMapper.java
+++ springboot-simple-example/src/main/java/com/hjq/springboot/springbootsimpleexample/dao/OrderMapper.java
@@ -1,9 +1,11 @@
 package com.hjq.springboot.springbootsimpleexample.dao;
 
+import com.hjq.springboot.springbootsimpleexample.model.OrderInfo;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface OrderMapper {
 
+    int insertOrder(OrderInfo orderInfo);
 
 }
